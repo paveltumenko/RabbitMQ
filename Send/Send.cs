df768198cb0a53699e918851507f10be7d5ebff6
Pavel Tumenko <pavel.tumenko@gmail.com>
2018-02-25 18:44:49 +0200

    Hello World

---
+++ Send/Send.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "Hello World!";
+            Send(message);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        static void Send(string message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+    }
+}
